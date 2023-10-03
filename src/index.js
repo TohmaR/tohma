7b1184dae3e8bd7e3608981d50f03c792c83d910
Tohma <praxyftw@gmail.com>
2023-10-03 15:46:12 +0200

    add service worker

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, } from "react-router-dom"; 
 import './index.css';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -12,3 +13,5 @@ root.render(
     </Router>
   </React.StrictMode>,
 );
+
+serviceWorkerRegistration.register();
